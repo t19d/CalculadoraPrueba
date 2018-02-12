78d2b3a74af59d4c7364fbfc4bf224c4b4694415
t19d <33786007+t19d@users.noreply.github.com>
2018-02-12 13:39:43 +0100

    Add files via upload

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+public class Calculadora {
+
+    public static void ImprimirMenu() {
+        System.out.println("**************************"
+                + "\n*          MENU          *"
+                + "\n**************************"
+                + "\n*                        *"
+                + "\n*   -SUMA                *"
+                + "\n*   -RESTA               *"
+                + "\n*   -MULTIPLICACIÓN      *"
+                + "\n*   -DIVISIÓN            *"
+                + "\n**************************");
+    }
+}
